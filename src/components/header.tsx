e77362a107b04fc8b003e11e1a6363d8bf24e6a3
Vladimir <zajtzeffvl4d@yandex.ru>
2025-09-05 23:07:43 +0300

    Refactor: добавил форматер и исправил header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
-import logoDelivery from '../assets/svg/logo.svg'
+import logoDelivery from "../assets/svg/logo.svg";
 import Button from "./ui/button/button.tsx";
 
 const Header = () => {
-  const isUserLogin: boolean = false
+  const isUserLogin: boolean = false;
 
   return (
     <header className="flex items-center justify-between mb-10">
@@ -12,28 +12,26 @@ const Header = () => {
       <label className="flex-[0.8]">
         <input
           type="text"
-          className="bg-white border border-[#d9d9d9] rounded-[2px] text-base/[24px] px-2 pr-2 pl-[35px] bg-no-repeat bg-[left_11px_center] w-full bg-[url(../assets/svg/home.svg)]"
+          className="bg-white border border-[#d9d9d9] rounded-[2px] text-base/[24px] px-2 pr-2 pl-[35px] bg-no-repeat bg-[left_11px_center] w-full bg-[url(./assets/svg/home.svg)]"
           placeholder="Адрес доставки"
         />
       </label>
       <div className="flex items-center">
-        {isUserLogin && <span className="user-name"></span>}
-          <Button className="bg-[#1890ff] border-[#1890ff] text-white mr-[10px] hover:bg-white hover:border-[#d9d9d9] hover:text-[#595959] hover:duration-250">
-              Войти
-          </Button>
+        <Button className="bg-[#1890ff] border-[#1890ff] text-white mr-[10px] hover:bg-white hover:border-[#d9d9d9] hover:text-[#595959] hover:duration-250">
+          Войти
+        </Button>
         {isUserLogin && (
-            <Button className="mx-[5px]">
-                Корзина
-            </Button>
-        )}
-        {isUserLogin && (
-          <Button className="mx-[5px] bg-[#1890ff] border-[#1890ff] text-white mr-[10px] hover:bg-white hover:border-[#d9d9d9] hover:text-[#595959] hover:duration-250">
+          <>
+            <span className="mr-[20px] font-bold text-[18px]"></span>
+            <Button className="mx-[5px]">Корзина</Button>
+            <Button className="mx-[5px] bg-[#1890ff] border-[#1890ff] text-white mr-[10px] hover:bg-white hover:border-[#d9d9d9] hover:text-[#595959] hover:duration-250">
               Выйти
-          </Button>
+            </Button>
+          </>
         )}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
